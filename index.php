83c413e79e40ab1b3b6eed15007350475ede3f86
Nic Alexander <wuvykins@gmail.com>
2019-02-11 23:23:25 -0500

    slide 2 utilizing an insert query

--- index.php
+++ index.php
@@ -6,13 +6,30 @@
  * Time: 8:00 PM
  */
 //connect to DB
+require '/home/nalexand/config.php';
 try{
     //instantiate a database object
-    $dbh = new PDO("mysql:dbname=nalexand_grc",
-    "nalexand", "@lexander1");
+    $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
     echo 'connected to database!';
 }
 catch(PDOException $e)
 {
     echo $e->getMessage();
 }
+//define the query
+$sql= "INSERT INTO pets(type,name, color)
+        VALUES(:type,:name, :color)";
+
+//Prepare the statement
+$statement = $dbh->prepare($sql);
+
+//Bind the parameters
+$type = 'kangaroo';
+$name = 'Joey';
+$color = 'purple';
+$statement->bindParam(':type', $type, PDO::PARAM_STR);
+$statement->bindParam(':name', $name, PDO::PARAM_STR);
+$statement->bindParam(':color', $color, PDO::PARAM_STR);
+
+//Execute
+$statement->execute();
